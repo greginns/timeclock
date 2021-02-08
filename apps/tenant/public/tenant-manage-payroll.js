198cb3585fdd1e010cf55b6ea9d23dc3498dc4b5
greginns <greginns@users.noreply.github.com>
2021-02-08 14:16:02 -0400

    History report

--- apps/tenant/public/tenant-manage-payroll.js
+++ apps/tenant/public/tenant-manage-payroll.js
@@ -152,3 +152,97 @@ App.mvcObjs.tenant_manage_payroll = {
     }
   }
 }
+
+App.mvcObjs.tenant_manage_history = {
+  model: {
+    config: {},
+		hasRun: false,
+  },
+
+  lifecycle: {
+    created: function() {
+      var self = this;
+			this.getDepartments();
+			
+      return new Promise(function(resolve) {
+        resolve();  
+      })
+    },
+
+    ready: function() {
+    },
+
+    inView: async function(params) {
+      var self = this;
+      
+      this.startUp();
+    },
+
+    outView: function() {
+    }
+  },
+
+  controllers: {
+    run: async function() {
+      var emp = this.$get('emp');
+      var sdate = this.$get('sdate');
+      var edate = this.$get('edate');
+      
+      var res = await io.get({emp, sdate, edate}, '/tenant/history/run');
+      
+      if (res.status == 200) {
+				this.$set('hasRun', true);
+        this.$set('report', res.data);
+      }
+    },
+    
+    share: async function() {
+      var config = this.$get('config');
+      var source = this.$get('report');
+      var emp = this.$get('emp');
+      var filename = `history-${emp}-${this.$get('startDate')}-${this.$get('endDate')}`;
+
+      App.modals.share({config, source, filename});      
+    },
+
+    startUp: async function() {
+      var ret = await io.get({}, '/tenant/payroll/params');
+
+      if (ret.status == 200) {
+        this.$set('config', ret.data.config);
+      }
+    },
+		
+    getDepartments: function() {
+			var self = this;
+			
+			io.get({}, '/tenant/department')
+			.then(function(res){
+				if (res.status == 200) {
+          console.log(res.data)                    
+          self.$set('depts', res.data);
+          self.$set('dept', res.data[0].code);
+				}
+			})
+    },
+
+		getEmployees: function() {
+      var self = this;
+      var dept = this.$get('dept');
+			
+			io.get({active: 'Y'}, '/tenant/employee/department/' + dept)
+			.then(function(res){
+				if (res.status == 200) {
+          self.$set('employees', res.data);
+          self.$set('emp', res.data[0].code)
+				}
+			})
+		},
+  },
+
+  watch: {
+    'dept': function(nv) {
+      this.getEmployees();
+    }
+  }
+}
